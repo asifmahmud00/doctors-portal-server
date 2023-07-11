139b37c4173f75f1cbb23620971f42b16868cd6d
asifmahmud00 <asif.mahmud00@gmail.com>
2023-07-11 23:05:26 +0600

    3rd commit server

--- index.js
+++ index.js
@@ -32,14 +32,87 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            console.log(date);
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
+            const bookingQuery = { appointmentDate: date };
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                option.slots = remainingSlots;
+                console.log(date, option.name, remainingSlots.length);
+            })
+
             res.send(options);
         })
+
+
+        app.get('/v2/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        });
+
+
+
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
+            const query = {
+                email: booking.email,
+                appointmentDate: booking.appointmentDate,
+                treatment: booking.treatment
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`;
+                return res.send({ acknowledged: false, message })
+            }
+
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
@@ -62,3 +135,5 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Doctor portal server running on ${port}`)
 })
+
+
